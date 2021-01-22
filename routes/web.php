50a27cf588a9a91d6cb083276817943c4af4ed77
JENNY <48593166+jenny111788@users.noreply.github.com>
2021-01-22 16:11:59 +0800

    Day3-4 Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cats/{id}/{name}', function($id, $name){
+    return "我是第" . $id . "號的貓：" . $name;
+});
