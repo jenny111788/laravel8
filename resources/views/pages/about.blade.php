d6e566d32d41ef329c2d55721a12c3b096e3bca0
JENNY <48593166+jenny111788@users.noreply.github.com>
2021-02-04 17:32:53 +0800

    Day7 Layouts

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,15 +1,21 @@
-<h1>Hi</h1>
+@extends('layouts/application')
 
-@if (count($heroes) > 0)
-    @foreach ($heroes as $hero)
-        @if ($loop->first || $loop->last)
-            <li>
-                <strong>{{ $hero }}</strong>
-            </li>
-        @else
-            <li>
-                {{ $hero }}
-            </li>
-        @endif
-    @endforeach
-@endif
+@section('title', 'Learn to Laravel')
+
+@section('main')
+    <h1>Hi</h1>
+
+    @if (count($heroes) > 0)
+        @foreach ($heroes as $hero)
+            @if ($loop->first || $loop->last)
+                <li>
+                    <strong>{{ $hero }}</strong>
+                </li>
+            @else
+                <li>
+                    {{ $hero }}
+                </li>
+            @endif
+        @endforeach
+    @endif
+@endsection
