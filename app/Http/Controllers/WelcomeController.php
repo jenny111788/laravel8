8b945691032268193c6831dd6276b17c9ad0f73b
JENNY <48593166+jenny111788@users.noreply.github.com>
2021-02-04 16:07:10 +0800

    Day6 Views

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,6 +8,9 @@ class WelcomeController extends Controller
 {
     public function about() {
         // return "hi";
-        return view('pages.about');
+        // $name = "復仇者聯盟";
+        // $js = "<script>alert('hi');</script>";
+        $heroes = ["鋼鐵人", "美國隊長", "綠巨人浩克", "索爾", "洛基", "黑寡婦", "鷹眼"];
+        return view('pages.about', ['heroes' => $heroes]);
     }
 }
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,1 +1,15 @@
 <h1>Hi</h1>
+
+@if (count($heroes) > 0)
+    @foreach ($heroes as $hero)
+        @if ($loop->first || $loop->last)
+            <li>
+                <strong>{{ $hero }}</strong>
+            </li>
+        @else
+            <li>
+                {{ $hero }}
+            </li>
+        @endif
+    @endforeach
+@endif
