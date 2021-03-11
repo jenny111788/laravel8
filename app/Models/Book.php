f7ee869beee92a8ff3341e701344adb242ed5a32
JENNY <48593166+jenny111788@users.noreply.github.com>
2021-03-11 17:15:07 +0800

    Day17 Model 關連性-多對多

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -16,4 +16,8 @@ class Book extends Model
     public function store() {
         return $this->belongsTo('App\Models\Store');
     }
+
+    public function authors() {
+        return $this->belongsToMany('App\Models\Author');
+    }
 }
